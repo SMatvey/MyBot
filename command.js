a8679e1f3d081af2a7af922e5bece184b412585e
Matvey <matveysvischo2004@gmail.com>
2022-12-11 14:49:15 +0200

    -h1

---
+++ command.js
@@ -0,0 +1,8 @@
+const commands = `
+/start - перезапуск бота
+/help - список команд
+/music - робота з базою даних музики
+/search_music - різний пошук та сортування
+`
+
+module.exports.commands = commands;
--- index.js
+++ index.js
@@ -3,62 +3,140 @@ const Telegraf = require("telegraf").Telegraf,
     BOT_TOKEN = "5788385404:AAF7cm9VFmkMo8cjKXuhIZpo4ZB_jrWoYyg";
 
 const bot = new Telegraf(BOT_TOKEN);
+const { markAsUntransferable } = require("node:worker_threads");
+const { Markup } = require("telegraf");
+const comm = require('./command.js')
 
-let data_from_server={};
-let data_of_data_from_server="";
+// let data_from_server={};
+// let data_of_data_from_server="";
 
-function getCurrentDate()
-{
-    var today=new Date();
-    var today_day = String(today.getDate()).padStart(2,'0');
-    var today_month = String(today.getMonth()+1).padStart(2,'0');
-    var today_year = today.getFullYear();
-    today = today_year + "-" + today_month + "-" + today_day;
-    console.log(today);
-    console.log(data_of_data_from_server);
-    return today;
-}
+// function getCurrentDate()
+// {
+//     var today=new Date();
+//     var today_day = String(today.getDate()).padStart(2,'0');
+//     var today_month = String(today.getMonth()+1).padStart(2,'0');
+//     var today_year = today.getFullYear();
+//     today = today_year + "-" + today_month + "-" + today_day;
+//     console.log(today);
+//     console.log(data_of_data_from_server);
+//     return today;
+// }
 
-function getDataFromServer() {
-    return fetch("https://russianwarship.rip/api/v1/statistics/latest",{
-        method: "GET",
-        headers: {"Content-Type":"appication/json"}
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            data_from_server=data;
-            data_of_data_from_server=data_from_server.data.date;
-        })
-        .catch((er) => {
-            console.log('Error: $er');
-        })
-}
+// function getDataFromServer() {
+//     return fetch("http://127.0.0.1:3000/music",{
+//         method: "GET",
+//         headers: {"Content-Type":"appication/json"}
+//         })
+//         .then((res) => res.json())
+//         .then((data) => {
+//             data_from_server=data;
+//             data_of_data_from_server=data_from_server.data.date;
+//         })
+//         .catch((er) => {
+//             console.log('Error: $er');
+//         })
+// }
 
-bot.start(ctx=> (
-    ctx.reply("Вітаю!")
-));
+// bot.hears(/[A-Z]+/i, async (ctx) => { 
+//     let message = ctx.message.text;
+//     console.log(message);
+//     if(data_of_data_from_server!=getCurrentDate())
+//     {
+//         await getDataFromServer();
+//         ctx.reply(message +": "+data_from_server.data.stats[message]);
+//     }
+//     else
+//     {
+//         ctx.reply(message +": "+data_from_server.data.stats[message]);
+//         console.log("dont go to server");
+//     }
+// })
 
-bot.hears(/[A-Z]+/i, async (ctx) => {
-    let message = ctx.message.text;
-    console.log(message);
-    if(data_of_data_from_server!=getCurrentDate())
-    {
-        await getDataFromServer();
-        ctx.reply(message +": "+data_from_server.data.stats[message]);
-    }
-    else
-    {
-        ctx.reply(message +": "+data_from_server.data.stats[message]);
-        console.log("dont go to server");
+bot.start((ctx) => {
+    ctx.reply(`Вітаю, ${ctx.message.from.first_name}! Готовий до роботи.`);
+})
+
+bot.help((ctx) => {
+    ctx.reply(comm.commands)
+})
+
+bot.hears([/Привіт+/i, /Hi+/i,/Hello+/i], ctx=> {
+    ctx.reply("Й тобі привіт.")
+})
+
+bot.command('music', async (ctx) => { 
+    try {
+        await ctx.replyWithHTML('<b>Виберіть команду для запуску</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Показати лист всіх пісень','btn_getMusic'), Markup.button.callback('Додати пісню до бази','btn_postMusic')],
+                [Markup.button.callback('Знайти по айді','btn_getMusicId'), Markup.button.callback('Оновити по айді','btn_putMusicId'), 
+                Markup.button.callback('Видалити по айді','btn_delMusicId')],
+                [Markup.button.callback('Відсортувати за алфавітом','btn_getMusicAZ'), 
+                Markup.button.callback('Відсортувати в зворотньому напрямку','btn_getMusicZA')]
+            ]
+        )) 
+    } catch(e) {
+        console.error(e)
     }
 })
 
-bot.hears(/Привіт+/i, ctx=> {
-    ctx.reply("Й тобі привіт")
+bot.command('search_music', async (ctx) => { 
+    try {
+        await ctx.replyWithHTML('<b>Виберіть команду для запуску</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Порахувати всі пісні','btn_coutMusic'), Markup.button.callback('Порахувати пісні автора','btn_coutMusicA')],
+                [Markup.button.callback('Випадкова пісню','btn_getMusicRand')],
+                [Markup.button.callback('Пісні за частковим автором','btn_getMusicPartAuthor'), 
+                Markup.button.callback('Пісні за частковую назвою','btn_getMusicPartSong')],
+                [Markup.button.callback('Знайти пісню за автором','btn_getMusicAuthor'), 
+                Markup.button.callback('Знайти пісню за назвою','btn_getMusicSong')]
+            ]
+        )) 
+    } catch(e) {
+        console.error(e)
+    }
 })
 
-bot.hears(["Hi","Hello"], ctx=> (
-    ctx.reply("\u{1F600}")
-));
+// function getMusic() {
+//     return fetch("http://127.0.0.1:3000/music",{
+//         method: "GET",
+//         headers: {"Content-Type":"appication/json"}
+//         })
+//         .then((res) => res.json())
+//         .then((data) => {
+//             data_from_server=data;
+//             data_of_data_from_server=data_from_server.data.date;
+//         })
+//         .catch((er) => {
+//             console.log('Error: $er');
+//         })
+// }
 
-bot.launch();
+bot.action('btn_getMusic', async (ctx) => { 
+    try {
+        // fetch("http://127.0.0.1:3000/music",{
+        // method: "GET",
+        // headers: {"Content-Type":"appication/json"}
+        // })
+        // .then((res) => res.json())
+        
+
+        // .catch((er) => {
+        //     console.log('Error: $er');
+        // })
+        
+    } catch(e) {
+        console.error(e)
+    }
+})
+
+
+bot.on('message', (ctx) => { 
+    ctx.reply("Схоже ви не вибрали корректну команду. Спробуйте будь-ласка ще раз!")       
+})
+
+console.log('all alright')
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
